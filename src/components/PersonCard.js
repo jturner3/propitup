e821817dd26ac24e289cb41727fe00f171c360ac
jturner3 <43verventures.io>
2021-08-14 18:18:16 -0400

    second commit

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -15,7 +15,6 @@ const ageClickHandler = (event) => {
             <h2>{lastName} , {firstName}</h2>
             <p>Age: {personAge}</p>
             <p>Hair Color: {hairColor}</p>
-            <button onClick = {(event) => ageClickHandler (event)}>Birthday Button for {lastName} {firstName}</button>
         </div>
     )
 }
